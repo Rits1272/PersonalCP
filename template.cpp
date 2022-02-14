018a5cb3b55a0f7d9f1cda6aa31e5a82f45b4bd6
Ritik Jain <ritikjain1272@gmail.com>
2022-02-14 19:58:36 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,32 @@
+/*
+ * author : rits1272
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define print(x) cerr << "[" << #x << ": " << x << "]";
+#define println(x) cerr << "[" << #x << ": " << x << "]\n";
+#define show(v) for(int i = 0; i < (int)v.size(); i++) cout << v[i] << (i == (int)v.size()-1 ? "\n" : " ");
+#define deb(v) for(int i = 0; i < (int)v.size(); i++) {print(i) println(v[i])}
+#define pb push_back
+#define INF 1e18 + 5
+//int dx[] = { -1, 1, 0, 0 }; 
+//int dy[] = { 0, 0, 1, -1 }; 
+ 
+const int MOD = 1e9 + 7;
+
+void test_case(){
+
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+	int tt = 1; 
+	cin >> tt;
+	while(tt--){
+	    test_case();
+	}
+	return 0; 
+}
